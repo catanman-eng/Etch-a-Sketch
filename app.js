936af07675b532656f4274d22a918871cebf2743
Evan Catania <evan.catania@gmail.com>
2021-07-04 22:01:03 -0400

    Added reset functionality with prompt to change grid size on reset

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const container = document.getElementById("container");
+const grid = document.getElementById("grid")
+const reset = document.getElementById("reset");
+const colorPicker = document.getElementById("color-select")
 
 window.addEventListener("load", defaultGrid);
 
@@ -15,17 +17,38 @@ makeGrid = (rows, cols) => {
     let cell = document.createElement("div");
     cell.className = "cells";
     cell.addEventListener("mouseover", changeColor);
-    container.appendChild(cell);
+    grid.appendChild(cell);
   };
 
 };
+
+
+function userColorSelection(event) {
+  color = event.target.value;
+}
+
+
 
 function changeColor(e) {
   const randR = Math.floor(Math.random() *256);
   const randG = Math.floor(Math.random() *256);
   const randB = Math.floor(Math.random() *256);
 
-  e.target.style.backgroundColor = `rgb(${randR}, ${randG}, ${randB})`
+  e.target.style.backgroundColor = `rgb(${randR},${randG},${randB})`;
+  // e.target.style.backgroundColor = color;
 }
 
+reset.addEventListener("click", resetGrid);
 
+function resetGrid() {
+  let allCells = document.querySelectorAll(".cells").forEach(cell => {
+    cell.style.backgroundColor = "white";
+  });
+
+  let size = prompt("Please Select New Grid Size (Maximum:100", "50");
+
+  while (grid.firstChild) grid.removeChild(grid.firstChild);
+
+  makeGrid(`${size}`, `${size}`);
+
+}
